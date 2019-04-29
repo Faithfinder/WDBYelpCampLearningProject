3fd8eb8308db44fb9a50257169e0335a4794628c
Faithfinder <faithfinder@yandex.ru>
2019-04-29 15:43:25 -0400

    Header, footer, bootstrap

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds");
+    var sampleCampgrounds = [
+        {name: "Campground 1", image: "https://farm8.staticflickr.com/7285/8737935921_47343b7a5d.jpg"},
+        {name: "Campground 2", image: "https://farm9.staticflickr.com/8471/8137270056_21d5be6f52.jpg"},
+        {name: "Campground 3", image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg"}
+    ];
+    res.render("campgrounds", {campgrounds: sampleCampgrounds});
 });
 
 app.listen(
