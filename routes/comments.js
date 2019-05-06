84a919f13cc8a5560802f81e142aa289a20e6a3f
Faithfinder <faithfinder@yandex.ru>
2019-05-06 11:58:41 -0400

    Comments user association

--- routes/comments.js
+++ routes/comments.js
@@ -23,12 +23,15 @@ router.post("/", isLoggedIn, (req, res) => {
                 if (err) {
                     console.log("Error!", err);
                 } else {
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
+
                     campground.comments.push(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id);
                 }
             });
-
         }
     });
 });
